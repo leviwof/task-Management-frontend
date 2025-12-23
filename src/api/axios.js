70c6f08355eb3ed37806f78560c6af6840917b5a
leviwof <ganeshkemoap123@gmail.com>
2025-12-23 18:43:16 +0530

    frontend updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+    // baseURL: "http://localhost:5000/api"
+    baseURL: "https://task-management-system-j0k1.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
